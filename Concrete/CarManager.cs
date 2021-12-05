88153543d27fcf5a705e168b10ce4dfb0c02f53f
OmerFarukD <89133026+OmerFarukD@users.noreply.github.com>
2021-12-05 16:00:29 +0300

    Add files via upload

---
+++ Concrete/CarManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        private ICarDal cardal;
+        public CarManager(ICarDal cardal)
+        {
+            this.cardal = cardal;
+        }
+        public void Add(Car car)
+        {
+            this.cardal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            this.cardal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return this.cardal.GetAll();
+        }
+
+        public void Update(Car car)
+        {
+            this.cardal.Update(car);
+        }
+    }
+}
---
+++ Concrete/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Customer :IEntity
+    {
+        public string CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
